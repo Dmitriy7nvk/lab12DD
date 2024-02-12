e3e3031f865d99f5d6ce061625640e94223c7d01
dench <denchikgulin9@gmail.com>
2024-02-12 10:08:07 +0800

    Добавлен код

--- Program.cs
+++ Program.cs
@@ -11,10 +11,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Demo of Unit-testing in C#");
+            Console.WriteLine("Anime");
         }
 
         public static int Min(int a, int b, int c)
         {
+
             int min = a;
             if (min > b) min = b;
             if (min > c) min = c;
