1c4223fa642ee22ecf6d74e4e279d1694801ab1b
Dmitriy7nvk <dimanovokresh@gmail.com>
2024-02-12 10:02:54 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo of Unit-testing in C#");
+        }
+
+        public static int Min(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
+    }
+}
